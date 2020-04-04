a4141fc7b93d0cd56a805c36686305c5ddd12633
zhouboyu <zhouboyu@fangxiaoer.com>
2020-04-05 07:08:40 +0800

    firststept

--- src/router/index.js
+++ src/router/index.js
@@ -36,97 +36,17 @@ export const constantRouterMap = [
 
 export const asyncRouterMap = [
   {
-    path: '/pms',
+    path: '/importData',
     component: Layout,
-    redirect: '/pms/product',
-    name: 'pms',
-    meta: {title: '商品', icon: 'product'},
+    redirect: '/importData/index',
+    name: 'importData',
+    meta: {title: '数据', icon: 'product'},
     children: [{
-      path: 'product',
-      name: 'product',
-      component: () => import('@/views/pms/product/index'),
-      meta: {title: '商品列表', icon: 'product-list'}
-    },
-      {
-        path: 'addProduct',
-        name: 'addProduct',
-        component: () => import('@/views/pms/product/add'),
-        meta: {title: '添加商品', icon: 'product-add'}
-      },
-      {
-        path: 'updateProduct',
-        name: 'updateProduct',
-        component: () => import('@/views/pms/product/update'),
-        meta: {title: '修改商品', icon: 'product-add'},
-        hidden: true
-      },
-      {
-        path: 'productCate',
-        name: 'productCate',
-        component: () => import('@/views/pms/productCate/index'),
-        meta: {title: '商品分类', icon: 'product-cate'}
-      },
-      {
-        path: 'addProductCate',
-        name: 'addProductCate',
-        component: () => import('@/views/pms/productCate/add'),
-        meta: {title: '添加商品分类'},
-        hidden: true
-      },
-      {
-        path: 'updateProductCate',
-        name: 'updateProductCate',
-        component: () => import('@/views/pms/productCate/update'),
-        meta: {title: '修改商品分类'},
-        hidden: true
-      },
-      {
-        path: 'productAttr',
-        name: 'productAttr',
-        component: () => import('@/views/pms/productAttr/index'),
-        meta: {title: '商品类型', icon: 'product-attr'}
-      },
-      {
-        path: 'productAttrList',
-        name: 'productAttrList',
-        component: () => import('@/views/pms/productAttr/productAttrList'),
-        meta: {title: '商品属性列表'},
-        hidden: true
-      },
-      {
-        path: 'addProductAttr',
-        name: 'addProductAttr',
-        component: () => import('@/views/pms/productAttr/addProductAttr'),
-        meta: {title: '添加商品属性'},
-        hidden: true
-      },
-      {
-        path: 'updateProductAttr',
-        name: 'updateProductAttr',
-        component: () => import('@/views/pms/productAttr/updateProductAttr'),
-        meta: {title: '修改商品属性'},
-        hidden: true
-      },
-      {
-        path: 'brand',
-        name: 'brand',
-        component: () => import('@/views/pms/brand/index'),
-        meta: {title: '品牌管理', icon: 'product-brand'}
-      },
-      {
-        path: 'addBrand',
-        name: 'addBrand',
-        component: () => import('@/views/pms/brand/add'),
-        meta: {title: '添加品牌'},
-        hidden: true
-      },
-      {
-        path: 'updateBrand',
-        name: 'updateBrand',
-        component: () => import('@/views/pms/brand/update'),
-        meta: {title: '编辑品牌'},
-        hidden: true
-      }
+      path: 'importData',
+      name: 'importData',
+      component: () => import('@/views/importData/index'),
+      meta: {title: '数据列表', icon: 'product-list'}
+    }
     ]
   },
   {
---
+++ src/views/autoForm/autoForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-form :model="dynamicValidateForm" label-width="120px" ref="dynamicValidateForm">
+    <el-form-item :key="domain.prop"
+                  :label="domain.label"
+                  :prop="domain.prop"
+                  :rules="rules"
+                  v-for="domain in dynamicValidateForm.domains">
+      <el-input   type="money"
+                  :disabled="domain.editable"
+                  autocomplete="off"
+                  v-model.number="domain.value">
+      </el-input>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script>
+
+    export default {
+
+        name: "autoForm"
+    }
+</script>
+
+<style scoped>
+
+</style>
