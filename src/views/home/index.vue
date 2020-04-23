ff377ca681b268df31fbeafb8b8859e4ca985646
zhouboyu <zhouboyu@fangxiaoer.com>
2020-04-24 02:04:12 +0800

    首页图片

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,4 +1,23 @@
 <template>
-    <h1>hellow world!!!</h1>
+  <div>
+    <div class="app-container">
+      <el-col :span="12">
+        <img :src="src" alt="加油！！！" class="img-style" height="1227px"
+    width="1608px">
+      </el-col>
+    </div>
+  </div>
 </template>
 
+<script>
+  import home_index from '@/assets/images/home_index.jpg';
+
+  export default {
+    name:home_index,
+    data() {
+      return {
+        src: home_index
+      }
+    }
+  }
+</script>
